8b5930cec67efb275b9328a78f6146f165d69867
Gabriel <gabriel_henrique2805@hotmail.com>
2023-03-09 22:12:48 -0300

    finalizando o campo de criação de artigo

--- articles/articleController.js
+++ articles/articleController.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const category = require("../categories/category");
+const article = require("./article");
+const slugify = require("slugify");
 
 router.get("/admin/articles/new", (req, res)=>{
     category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories});
     });
+});
+
+router.post("/articles/save", (req,res) => {
+    let title = req.body.title;
+    let slug = slugify(title);
+    let body = req.body.body;
+    let categoryId = req.body.category;
+    
+    article.create({
+        title: title,
+        slug: slug,
+        body: body,
+        categoryId: categoryId
+    }).then(()=>{res.redirect("/")});
 });
 
 module.exports = router;
