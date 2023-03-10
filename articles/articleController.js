871c6f2b02e2b20e08e71612d4ed2386d7aaf0d3
Gabriel <gabriel_henrique2805@hotmail.com>
2023-03-09 21:21:34 -0300

    adicionando categorias no formulario de artigo

--- articles/articleController.js
+++ articles/articleController.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const category = require("../categories/category");
 
 router.get("/admin/articles/new", (req, res)=>{
-    res.render("admin/articles/new");
+    category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    });
 });
 
 module.exports = router;
