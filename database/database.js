8e237c112cadc9c1dbf3415fca8002640a3bde07
Gabriel <gabriel_henrique2805@hotmail.com>
2023-03-10 21:23:48 -0300

    ajustando o horario dos registro de artigo

--- database/database.js
+++ database/database.js
@@ -7,11 +7,13 @@ const user = "root";
 const password = "Lasanha2805g";
 const server = "localhost";
 const database = "mysql";
+const timeZone = "-03:00";
 
 // instanciando o objeto de conexão do banco
 const connection = new Sequelize(name, user, password, {
     host: server,
-    dialect: database
+    dialect: database,
+    timezone: timeZone
 });
 
 module.exports = connection;
